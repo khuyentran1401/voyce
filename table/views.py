090aeab872cc6132747769dfe4eaa74e1a4664ee
khuyentran1401 <khuyentran1476@gmail.com>
2020-05-11 13:14:05 -0500

    Second Commit

--- table/views.py
+++ table/views.py
@@ -1,7 +1,118 @@
 from django.shortcuts import render
+from table.models import Table
+import os
+import csv
 
 # Create your views here.
 # request is HttpRequestObject that is created whenever a page is loaded
 # render looks for HTML templates inside a directory called templates
+
 def view_table(request):
-	return render(request, 'table.html', {})
+
+	with open('../Desktop/voyce.csv') as csvfile:
+
+		next(csvfile)
+
+		data = csv.reader(csvfile, delimiter=',')
+
+		for items in data:
+			
+
+			facility = items[0]
+			type = items[1]
+			number_beds = items[2]
+			region = items[3]
+			address = items[4]
+			city = items[5]
+			state = items[6]
+			zip = items[7]
+			telefone = items[8]
+			fax = items[9]
+			admin = items[10]
+			admin_email = items[11]
+			sw = items[12]
+			sw_email = items[13]
+			markt = items[14]
+			markt_email = items[15]
+
+			user = Table.objects.get_or_create(facility=facility,
+				type=type,
+				number_beds=number_beds,
+				region=region,
+				address=address,
+				city=city,
+				state=state,
+				zip=zip,
+				telefone=telefone,
+				fax=fax,
+				admin=admin,
+				admin_email=admin_email,
+				sw=sw,
+				sw_email=sw_email,
+				markt=markt,
+				markt_email=markt_email)
+			
+	
+	users = Table.objects.all()
+
+	context = {
+		'users': users
+	}
+	return render(request, 'table.html', context)
+
+'''
+def printrow():
+	with open('../Desktop/voyce.csv') as csvfile:
+
+		next(csvfile)
+
+		data = csv.reader(csvfile, delimiter=',')
+
+		for items in data:
+
+			facility = items[0]
+			type = items[1]
+			number_beds = items[2]
+			region = items[3]
+			address = items[4]
+			city = items[5]
+			state = items[6]
+			zip = items[7]
+			telefone = items[8]
+			fax = items[9]
+			admin = items[10]
+			admin_email = items[11]
+			sw = items[12]
+			sw_email = items[13]
+			markt = items[14]
+			markt_email = items[15]
+
+			try:
+				user = Table.objects.get_or_create(facility=facility,
+					type=type,
+					number_beds=number_beds,
+					region=region,
+					address=address,
+					city=city,
+					state=state,
+					zip=zip,
+					telefone=telefone,
+					fax=fax,
+					admin=admin,
+					admin_email=admin_email,
+					sw=sw,
+					sw_email=sw_email,
+					markt=markt,
+					markt_email=markt_email)
+			except:
+				print(number_beds)
+			
+			print(user)
+			
+
+
+printrow()
+'''
+
+
+
--- voyce/settings.py
+++ voyce/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'voyce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'voyce',
+        'USER': 'khuyentran1401',
+        'PASSWORD': 'Cun123456', 
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
